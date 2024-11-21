073acfefeabeefcc634b767f639a57f9011c1727
ztc1997 <dev@ztc1997.com>
2024-11-21 21:30:58 +0800

    Use 15fps for SD

--- vector/src/main/java/im/vector/app/features/call/Cameras.kt
+++ vector/src/main/java/im/vector/app/features/call/Cameras.kt
@@ -19,5 +19,5 @@ data class CameraProxy(
 
 sealed class CaptureFormat(val width: Int, val height: Int, val fps: Int) {
     object HD : CaptureFormat(1280, 720, 30)
-    object SD : CaptureFormat(640, 480, 30)
+    object SD : CaptureFormat(640, 480, 15)
 }
